2e056b58235c56cabf638a374e64ca52229cbf7a
Chris Martin <ch.martin@gmail.com>
2020-08-29 13:56:39 -0600

    less brutalism

--- pixels.hs
+++ pixels.hs
@@ -1,36 +1,74 @@
+module Main (main) where
+
+import Relude
 import Control.Exception (bracket)
-import Control.Monad (forever)
-import Control.Monad.IO.Class (liftIO)
-import Control.Monad.Trans.Maybe (MaybeT (..))
 import qualified Graphics.X11 as X
 import qualified Graphics.X11.Xlib.Extras as X
 
+-- Everything that gets created before the main event loop starts.
+data Setup = Setup { atoms :: Atoms, display :: X.Display, window :: X.Window, eventPtr :: X.XEventPtr }
+
+-- All of the X atoms that we use in the program.
+data Atoms = Atoms { atom_WM_DELETE_WINDOW :: X.Atom, atom_WM_PROTOCOLS :: X.Atom }
+
+main :: IO ()
 main = withSetup eventLoop
 
-withSetup a = withDefaultDisplay \display -> createAtoms display >>= \atoms -> withWindow display \window -> setWMProtocols atoms display window *> X.mapWindow display window *> a atoms display
+withSetup :: (Setup -> IO a) -> IO a
+withSetup f =
+    withDefaultDisplay \display ->
+    createAtoms display >>= \atoms ->
+    withWindow display \window ->
+    X.allocaXEvent \eventPtr ->
+    Setup{ atoms, display, window, eventPtr } & \setup ->
+    setWMProtocols setup *>
+    X.mapWindow display window *>
+    f setup
 
-eventLoop atoms display = X.allocaXEvent \eventPtr -> runMaybeT $ forever $ liftIO (X.nextEvent display eventPtr) *> liftIO (X.getEvent eventPtr) >>= handleEvent atoms display
+eventLoop :: Setup -> IO ()
+eventLoop Setup{ display, eventPtr, atoms } =
+    fix \loop ->
+    X.nextEvent display eventPtr *>
+    X.getEvent eventPtr >>= \event ->
+    if | isQuitEvent atoms event -> pure ()
+       | otherwise -> loop
 
+withDefaultDisplay :: (X.Display -> IO a) -> IO a
 withDefaultDisplay = withDisplay ""
 
+withDisplay :: String -> (X.Display -> IO a) -> IO a
 withDisplay displayName = bracket (X.openDisplay displayName) X.closeDisplay
 
+withWindow :: X.Display -> (X.Window -> IO a) -> IO a
 withWindow display = bracket (createWindow display) (X.destroyWindow display)
 
-createWindow display = X.allocaSetWindowAttributes \attr -> X.createWindow display (X.defaultRootWindow display) 0 0 500 500 5 0 X.copyFromParent (defaultVisualOfDisplay display) 0 attr
+createWindow :: X.Display -> IO X.Window
+createWindow display =
+    X.allocaSetWindowAttributes \attr ->
+    X.defaultRootWindow display & \parent ->
+    defaultVisualOfDisplay display & \visual ->
+    X.createWindow display parent 0 0 500 500 5 0 X.copyFromParent visual 0 attr
 
-defaultVisualOfDisplay display = X.defaultVisualOfScreen (X.defaultScreenOfDisplay display)
+defaultVisualOfDisplay :: X.Display -> X.Visual
+defaultVisualOfDisplay = X.defaultVisualOfScreen . X.defaultScreenOfDisplay
 
-setWMProtocols Atoms{..} display window = X.setWMProtocols display window [wmDeleteWindow]
+setWMProtocols :: Setup -> IO ()
+setWMProtocols Setup{ atoms = Atoms{..}, display, window } =
+    X.setWMProtocols display window [atom_WM_DELETE_WINDOW]
 
-handleEvent Atoms{..} display = \case
-    X.ClientMessageEvent{ev_message_type, ev_data} | ev_message_type == wmProtocols, fmap toInteger (headMaybe ev_data) == Just (toInteger wmDeleteWindow) -> exitMaybeT
-    _ -> pure ()
+isQuitEvent :: Atoms -> X.Event -> Bool
+isQuitEvent
+    Atoms{ atom_WM_PROTOCOLS, atom_WM_DELETE_WINDOW }
+    X.ClientMessageEvent{ ev_message_type, ev_data } =
+        ev_message_type == atom_WM_PROTOCOLS &&
+        fmap toInteger (listToMaybe ev_data) == Just (toInteger atom_WM_DELETE_WINDOW)
+isQuitEvent _ _ = False
 
-headMaybe = \case x : _ -> Just x; [] -> Nothing
-
-exitMaybeT = MaybeT (pure Nothing)
-
-data Atoms = Atoms { wmDeleteWindow :: X.Atom, wmProtocols :: X.Atom }
-
-createAtoms display = let atom string = X.internAtom display string False in atom "WM_DELETE_WINDOW" >>= \wmDeleteWindow -> atom "WM_PROTOCOLS" >>= \wmProtocols -> pure Atoms{..}
+createAtoms :: X.Display -> IO Atoms
+createAtoms display =
+  do
+    atom_WM_DELETE_WINDOW <- atom "WM_DELETE_WINDOW"
+    atom_WM_PROTOCOLS <- atom "WM_PROTOCOLS"
+    pure Atoms{..}
+  where
+    atom string = X.internAtom display string False
