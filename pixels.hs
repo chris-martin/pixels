216124c478867e853a9b289858f4a74c831283cb
Chris Martin <ch.martin@gmail.com>
2020-08-29 14:29:44 -0600

    screwing around

--- pixels.hs
+++ pixels.hs
@@ -5,14 +5,11 @@ import Control.Exception (bracket)
 import qualified Graphics.X11 as X
 import qualified Graphics.X11.Xlib.Extras as X
 
--- Everything that gets created before the main event loop starts.
-data Setup = Setup { atoms :: Atoms, display :: X.Display, window :: X.Window, eventPtr :: X.XEventPtr }
-
--- All of the X atoms that we use in the program.
-data Atoms = Atoms { atom_WM_DELETE_WINDOW :: X.Atom, atom_WM_PROTOCOLS :: X.Atom }
-
 main :: IO ()
 main = withSetup eventLoop
+
+-- everything that gets created before the main event loop starts
+data Setup = Setup { atoms :: Atoms, display :: X.Display, window :: X.Window, eventPtr :: X.XEventPtr }
 
 withSetup :: (Setup -> IO a) -> IO a
 withSetup f =
@@ -26,11 +23,11 @@ withSetup f =
     f setup
 
 eventLoop :: Setup -> IO ()
-eventLoop Setup{ display, eventPtr, atoms } =
+eventLoop setup@Setup{ display, eventPtr } =
     fix \loop ->
     X.nextEvent display eventPtr *>
     X.getEvent eventPtr >>= \event ->
-    if | isQuitEvent atoms event -> pure ()
+    if | isQuitEvent setup event -> pure ()
        | otherwise -> loop
 
 withDefaultDisplay :: (X.Display -> IO a) -> IO a
@@ -56,19 +53,24 @@ setWMProtocols :: Setup -> IO ()
 setWMProtocols Setup{ atoms = Atoms{..}, display, window } =
     X.setWMProtocols display window [atom_WM_DELETE_WINDOW]
 
-isQuitEvent :: Atoms -> X.Event -> Bool
+isQuitEvent :: Setup -> X.Event -> Bool
 isQuitEvent
-    Atoms{ atom_WM_PROTOCOLS, atom_WM_DELETE_WINDOW }
-    X.ClientMessageEvent{ ev_message_type, ev_data } =
-        ev_message_type == atom_WM_PROTOCOLS &&
-        fmap toInteger (listToMaybe ev_data) == Just (toInteger atom_WM_DELETE_WINDOW)
+    Setup{ atoms = Atoms{ atom_WM_PROTOCOLS, atom_WM_DELETE_WINDOW } }
+    X.ClientMessageEvent{ ev_message_type = x, ev_data = listToMaybe -> Just y } =
+        x == atom_WM_PROTOCOLS && y `integerEq` atom_WM_DELETE_WINDOW
 isQuitEvent _ _ = False
+
+-- equality test between two different types of integers
+integerEq :: (Integral a, Integral b) => a -> b -> Bool
+integerEq a b = toInteger a == toInteger b
+
+-- all of the X atoms that we use in the program
+data Atoms = Atoms { atom_WM_DELETE_WINDOW :: X.Atom, atom_WM_PROTOCOLS :: X.Atom }
 
 createAtoms :: X.Display -> IO Atoms
 createAtoms display =
-  do
-    atom_WM_DELETE_WINDOW <- atom "WM_DELETE_WINDOW"
-    atom_WM_PROTOCOLS <- atom "WM_PROTOCOLS"
+    atom "WM_DELETE_WINDOW" >>= \atom_WM_DELETE_WINDOW ->
+    atom "WM_PROTOCOLS" >>= \atom_WM_PROTOCOLS ->
     pure Atoms{..}
   where
     atom string = X.internAtom display string False
