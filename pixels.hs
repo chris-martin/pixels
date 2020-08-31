891d5239ef8760ae0dc23a33030d3918d5f935bf
Chris Martin <ch.martin@gmail.com>
2020-08-31 03:17:11 -0600

    circle

--- pixels.hs
+++ pixels.hs
@@ -5,6 +5,8 @@ import Control.Exception (bracket)
 import qualified Graphics.X11 as X
 import qualified Graphics.X11.Xlib.Extras as X
 import Foreign.C.Types (CInt)
+import Foreign.Marshal.Alloc
+import Foreign.Storable
 import Data.Bits
 
 main :: IO ()
@@ -16,25 +18,54 @@ data Setup = Setup
   , display :: !X.Display
   , window :: !X.Window
   , eventPtr :: !X.XEventPtr
+  , image :: !X.Image
+  , gc :: !X.GC
   }
 
 withSetup :: (Setup -> IO a) -> IO a
 withSetup f =
     withDefaultDisplay \display ->
     createAtoms display >>= \atoms ->
+    pure (X.defaultScreenOfDisplay display) >>= \screen ->
+    pure (X.defaultVisualOfScreen screen) >>= \visual ->
     withWindow display \window ->
+    bracket (X.createGC display window) (X.freeGC display) \gc ->
 
     -- background
     bracket (X.createPixmap display window 2 2 depth) (X.freePixmap display) \bg ->
-    bracket (X.createGC display bg) (X.freeGC display) \gc ->
     X.setForeground display gc (shift 150 16 .|. shift 0 8 .|. 150) *>
     X.drawPoints display bg gc [X.Point 0 1, X.Point 1 0] X.coordModeOrigin *>
     X.setForeground display gc (shift 255 16 .|. shift 180 8 .|. 255) *>
     X.drawPoints display bg gc [X.Point 0 0, X.Point 1 1] X.coordModeOrigin *>
     X.setWindowBackgroundPixmap display window bg *>
 
+    -- initialize image
+    callocBytes (500 * 500 * 4) >>= \imageData -> -- freed when the image is destroyed
+    bracket (X.createImage display visual depth X.zPixmap 0 imageData 500 500 8 0) X.destroyImage \image ->
+
+    -- draw image
+    (
+      let
+        c1 = (150, 0, 150)
+        c2 = (255, 180, 255)
+      in
+        traverse_
+            (\(x, y) ->
+              let
+                dX = abs (fromIntegral x - 250) ^ 2
+                dY = abs (fromIntegral y - 250) ^ 2
+                s = sqrt (dX + dY)
+                (r, g, b) = if s > 60 && s < 65 then c1 else c2
+              in
+                pokeElemOff imageData (4 * (y * 500 + x) + 0) r *>
+                pokeElemOff imageData (4 * (y * 500 + x) + 1) g *>
+                pokeElemOff imageData (4 * (y * 500 + x) + 2) b
+            )
+            ((,) <$> [0..499] <*> [0..499])
+    ) *>
+
     X.allocaXEvent \eventPtr ->
-    Setup{ atoms, display, window, eventPtr } & \setup ->
+    pure Setup{ atoms, display, window, gc, image, eventPtr } >>= \setup ->
     setWMProtocols setup *>
     X.mapWindow display window *>
     f setup
@@ -45,7 +76,14 @@ eventLoop setup@Setup{ display, eventPtr } =
     X.nextEvent display eventPtr *>
     X.getEvent eventPtr >>= \event ->
     if | isQuitEvent setup event -> pure ()
+       | isExposeEvent event -> putImage setup *> loop
        | otherwise -> loop
+
+putImage :: Setup -> IO ()
+putImage Setup { display, window, gc, image } = X.putImage display window gc image 0 0 0 0 500 500
+
+isExposeEvent :: X.Event -> Bool
+isExposeEvent = \case X.ExposeEvent{} -> True; _ -> False
 
 withDefaultDisplay :: (X.Display -> IO a) -> IO a
 withDefaultDisplay = withDisplay ""
@@ -59,9 +97,10 @@ withWindow display = bracket (createWindow display) (X.destroyWindow display)
 createWindow :: X.Display -> IO X.Window
 createWindow display =
     X.allocaSetWindowAttributes \attr ->
-    X.defaultRootWindow display & \parent ->
-    X.defaultScreenOfDisplay display & \screen ->
-    X.defaultVisualOfScreen screen & \visual ->
+    X.set_event_mask attr X.exposureMask *> -- requires cwEventMask below
+    pure (X.defaultRootWindow display) >>= \parent ->
+    pure (X.defaultScreenOfDisplay display) >>= \screen ->
+    pure (X.defaultVisualOfScreen screen) >>= \visual ->
     X.createWindow display parent
         0 0 -- position
         500 500 -- size
@@ -69,7 +108,7 @@ createWindow display =
         depth
         X.inputOutput
         visual
-        0 -- which window attributes are defined in attr
+        X.cWEventMask -- which window attributes are defined in attr
         attr
 
 setWMProtocols :: Setup -> IO ()
